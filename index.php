6e0cafc8fdadcb0fc59fba61537f38bffd45a926
praveen28624 <68008375+praveen28624@users.noreply.github.com>
2022-07-27 16:45:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php include_once("a.mp4");
-$output = shell_exec('ls');
-echo "<pre>$output</pre>"; ?>
+shell_exec('apt install ffmpeg -y');
+sleep(60);
+shell_exec('ffmpeg -i https://lightning-traceurban-samsungau.amagi.tv/playlist.m3u8 -y -vn -b:a 64k -f hls -hls_list_size 5 -hls_time 2 -hls_delete_threshold 1 -hls_flags delete_segments trace.m3u8'); ?>
